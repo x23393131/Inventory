43124c52ebe32fa4a49ebacf3e6f2fda3d8d2679
EC2 Default User <ec2-user@ip-172-31-40-164.ec2.internal>
2025-04-04 16:13:19 +0000

    error musta;fhg;l

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inventory_system/.settings")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
